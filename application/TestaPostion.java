eb7ac854fc6e4a6e7eb24758fa360c81422faca2
Gilberto de Oliveira <gilbertouba@yahoo.com.br>
2020-07-22 08:33:37 -0300

    Classe Position + Teste

---
+++ application/TestaPostion.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class TestaPostion {
+    public static void main(String[] args) {
+         Position pos = new Position(1, 10);
+         System.out.println(pos.toString());
+    }    
+}
