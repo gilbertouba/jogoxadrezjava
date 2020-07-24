6c3e2c96a1bf99800766e5dac0b3486bab68a9de
Gilberto de Oliveira <gilbertouba@yahoo.com.br>
2020-07-24 11:08:39 -0300

    Montando o tabuleiro

--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.King;
+import chess.pieces.Rook;
+import boardgame.Position;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         this.board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][]  getPieces(){
@@ -22,7 +26,11 @@ public class ChessMatch {
         return mat;
 
     }
+    private void initialSetup(){
+        board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));  
+        board.placePiece(new King(board,Color.WHITE), new Position(0,4));  
 
+    }
     
     
 }
